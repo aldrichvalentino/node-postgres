d679f55e1e5ac74c031c0d42585870c7d977aaa8
aldrichvalentino <aldrich.vh97@gmail.com>
2018-05-08 22:29:38 +0700

    added sample tests

---
+++ src/tests/form.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Form from '../components/form';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Form />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
