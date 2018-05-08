51a3b9928ddbe0cc7046706fe52a2c5f10e7ec41
aldrichvalentino <aldrich.vh97@gmail.com>
2018-05-08 22:03:39 +0700

    added form and profile components

---
+++ src/components/form.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      birthday: '',
+    }
+  }
+
+  handleNameChange(e) {
+    this.setState({
+      username: e.target.value
+    });
+  }
+
+  handleDateChange(e) {
+    this.setState({
+      birthday: new Date(e.target.value)
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (this.state.birthday !== '' && this.state.username !== '') {
+      var data = {
+        username: this.state.username,
+        birthDate: this.state.birthday.getDate(),
+        birthMonth: this.state.birthday.getMonth(),
+        birthYear: this.state.birthday.getFullYear() 
+      };
+      axios.post('http://localhost:3000/users', data)
+      .then((response) => {
+        console.log(response.data);
+        this.props.update(response.data.username);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Register</h1>
+        <form onSubmit={this.handleSubmit.bind(this)} >
+          <div>
+            <input 
+              required
+              type="text" 
+              name="username" 
+              placeholder="username" 
+              onChange={this.handleNameChange.bind(this)} />
+          </div>
+          <div>
+            <input 
+              required
+              type="date" 
+              name="birthday" 
+              placeholder="birthday" 
+              onChange={this.handleDateChange.bind(this)} />
+          </div>
+          <div>
+            <input type="submit" value="register"/>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Form;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import Form from './components/form';
+import Profile from './components/profile';
 
 class App extends Component {
-    render() {
-        return (
-            <h1>Hello World</h1>
-        );
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: 'default'
     }
+  }
+
+  updateUser(newUser) {
+    this.setState({ user: newUser });
+  }
+
+  render() {
+    return (
+      <div>
+        <Form update={this.updateUser.bind(this)} />
+        <hr />
+        <Profile user={this.state.user} />
+      </div>
+    );
+  }
 }
 
 var mountNode = document.getElementById("app");
